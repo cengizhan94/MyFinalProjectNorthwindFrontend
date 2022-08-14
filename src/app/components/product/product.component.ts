f6cbc3b115ecec01e58079cc1a66d5bad3e8a12a
Cengiz Han <sonsair11@gmail.com>
2022-08-14 19:25:38 +0300

    product service added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http';
+import { ListResponseModel } from 'src/app/models/listResponseModel';
+import { ProductService } from 'src/app/services/product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -8,11 +12,36 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
 
+  apiUrl='https://localhost:44398/api/products/add'
   products:Product[] = [];
-  constructor() { }
+  dataLoaded = false;
+  constructor(private httpClient : HttpClient ,
+     private productService : ProductService, private activatedRoute : ActivatedRoute) { }
 
 
   ngOnInit(): void {
+   this.activatedRoute.params.subscribe(params=>{
+    if(params["categoryId"]){
+      this.getProductsByCategory(params["categoryId"])
+    }else{
+      this.getProducts()
+    }
+   })
+    
   }
+
+  getProducts(){
+    this.productService.getProduccts().subscribe(response =>{
+      this.products = response.data
+      this.dataLoaded = true;
+    })
+  }
+  
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response=>{
+      this.products = response.data
+    })
+  }
+
 
 }
