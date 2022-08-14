85cc1b2194d18c6288deca3aa8723cea9527368c
Cengiz Han <sonsair11@gmail.com>
2022-08-14 12:22:10 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { ProductComponent } from './components/product/product.component';
+import { CategoryComponent } from './components/category/category.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NaviComponent,
+    ProductComponent,
+    CategoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
