9cf8c1345b1e7dd3254935becfb8ace073744c5c
Cengiz Han <sonsair11@gmail.com>
2022-08-14 14:03:23 +0300

    category and product models added

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
+  products:Product[] = [];
   constructor() { }
+
 
   ngOnInit(): void {
   }
