af1806d4b6d730370dde2906cafa54cafe04b996
cengiz94 <sonsair11@gmail.com>
2022-08-16 06:41:36 +0300

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,36 @@ import { AppComponent } from './app.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { ProductComponent } from './components/product/product.component';
 import { CategoryComponent } from './components/category/category.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { CartSummaryComponent } from './components/cart-summary/cart-summary.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NaviComponent,
     ProductComponent,
-    CategoryComponent
+    CategoryComponent,
+    ProductAddComponent,
+    CartSummaryComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      positionClass:"toast-bottom-right"
+    })
   ],
-  providers: [],
+  providers: [{
+    provide:HTTP_INTERCEPTORS,useClass:AuthInterceptor,multi:true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/product-add/product-add.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-add',
+  templateUrl: './product-add.component.html',
+  styleUrls: ['./product-add.component.css']
+})
+export class ProductAddComponent implements OnInit {
+  productAddForm:FormGroup;
+
+  constructor(private formBuilder:FormBuilder,private productService:ProductService,private toastrService : ToastrService) { }
+
+  ngOnInit(): void {
+    this.createProductAddForm();
+  }
+
+  createProductAddForm(){
+    this.productAddForm = this.formBuilder.group({
+      productName : ["",Validators.required],
+      unitPrice:["",Validators.required],
+      unitsInStock:["",Validators.required],
+      categoryId:["",Validators.required]
+    })
+
+    }
+
+    add(){
+      if(this.productAddForm.valid){
+        let pproductModel = Object.assign({},this.productAddForm.value)
+        this.productService.add(pproductModel).subscribe(response=>{
+          this.toastrService.success(response.message,"Success")
+        },responseError=>{
+          if(responseError.error.Errors.length>0){
+            for(let i = 0;  i< responseError.error.Errors.length; i++){
+              this.toastrService.error(responseError.error.Errors[i].ErrorMessage,"Validation Error")
+            }
+          }
+        })
+      }else{
+        this.toastrService.error("Form is missing","Warning!")
+      }
+    }
+
+  }
+
+
